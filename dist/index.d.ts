daa24970ef3e059718c04d55da29ac963956fe71
chungnt <chungnt@pingcom.vn>
2017-10-14 09:08:14 +0700

    Remove build directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { AngularMultiSelect } from './angular2-multiselect-dropdown/multiselect.component';
-export { ClickOutsideDirective } from './angular2-multiselect-dropdown/clickOutside';
-export { ListFilterPipe } from './angular2-multiselect-dropdown/list-filter';
-export { AngularMultiSelectModule } from './angular2-multiselect-dropdown/multiselect.component';
